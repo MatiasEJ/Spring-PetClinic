979b465b38e5e9c8392252f166329ddb90ef3f10
MeJ <don.juncos@gmail.com>
2020-08-18 13:43:26 -0300

    listed all owners. Closes #16

--- pet-clinic-web/src/main/java/mej/springframework/petclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/mej/springframework/petclinic/controllers/OwnersController.java
@@ -1,14 +1,24 @@
 package mej.springframework.petclinic.controllers;
 
+import mej.springframework.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @RequestMapping("/owners")
 public class OwnersController {
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/", "/index","/index.html"})
-    private String listOwners(){
+    private String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
